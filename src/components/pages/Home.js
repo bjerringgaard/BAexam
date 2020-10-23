f460024913463b91d83dc7456fd0f9ef281f0025
Peter Wind <peterschaadtwind@gmail.com>
2020-10-23 12:25:51 +0200

    more components added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import ExpoItems from '../layout/ExpoItems';
 //import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './Home.scss';
 //import HomePage from './components/pages/Home';
 
 function Home() {
   return (
-      <div className="grid-x">
-          <p>MAIN CONTENT HOME</p>
+      <div className="grid-x containerHome">
+          <div className="cell"><p>HOME</p></div>
+          <div className="cell">
+            <ExpoItems />
+          </div>
+          
       </div>
   );
 }
