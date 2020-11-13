1626605a2bd5e1f5ea2d9dd668a850080d7f36c2
Peter <thewoet@gmail.com>
2020-11-13 09:55:45 +0100

    admin design

--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -2,21 +2,53 @@ import React, {useContext} from 'react';
 import './Admin.scss';
 import firebase from '../../Firebase'
 import { useAuth } from "../../Auth";
-
+import { RiDeleteBinLine } from 'react-icons/ri';
+import { FiRefreshCw } from 'react-icons/fi';
 
 function Admin() { 
 
     const {currentUser} = useAuth();
 
   return (
-      <div className="grid-x main-area">
-          <div className="cell auto admin-component">
-              <h1>Admin Panel</h1> TESTR
+
+      <div className="admin-component main-area">
+
+      <div className="grid-x">
+          <div className="cell auto">
+              <h1>Admin Panel</h1>
               <p className="welcome">Velkommen <strong>{currentUser.email}</strong></p>
               <p>Fusce molestie vestibulum ligula id facilisis. Mauris dapibus neque a neque eleifend, ac egestas lectus vestibulum. Praesent id lobortis odio. Donec suscipit massa sed elit consectetur efficitur. Vestibulum suscipit lacinia eros, eu imperdiet ex ultrices eu. Mauris ligula dolor, imperdiet et eleifend quis, viverra ut eros. Fusce ac magna tortor. Nulla quis accumsan tellus, et elementum odio. Cras finibus, turpis eu semper pharetra, justo elit gravida magna, eu convallis velit dui sit amet turpis. Donec posuere tristique sapien ac pulvinar. Pellentesque ut congue nulla. Pellentesque vel interdum sem.</p>
               <button onClick={() => firebase.auth().signOut()}>Sign Out</button>  
           </div>
+    </div>
+
+    <div className="grid-x admin-panel">
+        <div className="cell small-6">Users</div>
+            <div className="cell small-6">Owners</div>
+            <div className="grid-x admin-panel__content">
+                <div className="cell small-6">
+                    <ul>
+                        <li>Bruger 1</li>
+                        <li>Bruger 1</li>
+                        <li>Bruger 1</li>
+                    </ul>
+                </div>
+            <div className="cell small-6">
+
+                <div className="grid-x">
+                    <div className="cell auto">Bruger 1</div>
+                    <div className="cell auto icons"><RiDeleteBinLine /> <FiRefreshCw /></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
       </div>
+
+  
+
+   
   );
 }
 
