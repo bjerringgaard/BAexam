d69585e3bcf0514a1d291c71d9ae567634e60b45
Peter <thewoet@gmail.com>
2020-11-16 14:05:27 +0100

    user added to admin board

--- src/components/pages/AdminView.js
+++ src/components/pages/AdminView.js
@@ -7,9 +7,8 @@ import firebase from '../../Firebase'
 export default function AdminView() {
 
     const [accounts, setAccounts] = useState([])
-
     useEffect(() =>{
-        firebase
+        const unsubscribe = firebase
         .firestore()
         .collection('accounts')
         .where('company', '==', true)
@@ -20,55 +19,65 @@ export default function AdminView() {
             }))
             setAccounts(newAccount)
         })
-        
+        return () => unsubscribe
+    },[])
+
+    const [users, setUsers] = useState([])
+    useEffect(() =>{
+        const unsubscribe = firebase
+        .firestore()
+        .collection('accounts')
+        .where('user', '==', true)
+        .onSnapshot((snapshot) => {
+            const newUser = snapshot.docs.map((doc) =>({
+                id: doc.id,
+                ...doc.data()
+            }))
+            setUsers(newUser)
+        })
+        return () => unsubscribe
     },[])
 
 
     return (
         <div>
-       
-             
-        <div>
-        <div className="grid-x admin-panel">
-        <div className="cell small-6 admin-panel__header">
-            <div class="grid-x">
-                <div className="cell small-6 admin-panel__text">Users</div>
-                <div className="cell small-6 admin-panel__create"><BsPersonPlus /></div>
-            </div>
-        </div>
+            <div>
+            <div className="grid-x admin-panel">
             <div className="cell small-6 admin-panel__header">
-            <div class="grid-x">
-                <div className="cell small-6 admin-panel__text">Owner</div>
-                <div className="cell small-6 admin-panel__create"><BsPersonPlus /></div>
+                <div class="grid-x">
+                    <div className="cell small-6 admin-panel__text">Users</div>
+                    <div className="cell small-6 admin-panel__create"><BsPersonPlus /></div>
+                </div>
             </div>
-            </div>
-            <div className="grid-x admin-panel__content">
-                <div className="cell small-6 admin-panel__owners">
-                    <div className="grid-x admin-panel__information">
+                <div className="cell small-6 admin-panel__header">
+                <div class="grid-x">
+                    <div className="cell small-6 admin-panel__text">Owner</div>
+                    <div className="cell small-6 admin-panel__create"><BsPersonPlus /></div>
+                </div>
+                </div>
+                <div className="grid-x admin-panel__content">
+
+                    <div className="cell small-6 admin-panel__owners">
                     {accounts.map(account => (
-                        <div key={account.id}>
-                            <div className="cell small-12">{account.name} <RiDeleteBinLine /> <FiRefreshCw /></div><br></br>
+                        <div className="grid-x admin-panel__information" key={account.id}>
+                                <div className="cell small-10">{account.name}</div>
+                                <div className="cell auto icons"><RiDeleteBinLine /> <FiRefreshCw /></div>
                         </div>
                         ))}
                     </div>
-                </div>
-            <div className="cell small-6 admin-panel__users">
-                <div className="grid-x admin-panel__information">
-                    <div className="cell auto">Owner 1</div>
-                    <div className="cell auto icons"><RiDeleteBinLine /> <FiRefreshCw /></div>
-                </div>
-                <div className="grid-x admin-panel__information">
-                    <div className="cell auto">Owner 2</div>
-                    <div className="cell auto icons"><RiDeleteBinLine /> <FiRefreshCw /></div>
-                </div>
+                    
+                    <div className="cell small-6 admin-panel__users">
+                    {users.map(user => (
+                        <div className="grid-x admin-panel__information" key={user.id}>
+                                <div className="cell small-10">{user.fname}</div>
+                                <div className="cell auto icons"><RiDeleteBinLine /> <FiRefreshCw /></div>
+                        </div>
+                        ))}
+                    </div>
+
             </div>
-        </div>
-        </div>
-        </div>
-
-            
-
-
+            </div>
+            </div>
         </div>
     )
 }
