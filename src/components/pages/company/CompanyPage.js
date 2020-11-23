fe09489021619255051eb72e6121abc423beb5a8
Marcus Bjerringgaard <mba-98@live.dk>
2020-11-23 13:39:15 +0100

    Dynamisk Messe og messe items

--- src/components/pages/company/CompanyPage.js
+++ src/components/pages/company/CompanyPage.js
@@ -17,13 +17,49 @@ function CompanyPage() {
 
   const [company, setCompany] = React.useState([]);
   const [companyData, setCompanyData] = React.useState([]);
+  const [messeData, setMesseData] = React.useState([]);
   const { companyID } = useParams();
 
   const [title, setTitle] = React.useState([]);
   const [desc, setDesc] = React.useState([]);
   const [file, setFile] = React.useState([]);
   const [messe, setMesse] = React.useState([]);
-  
+
+  // Read Account
+  React.useEffect(() => {  
+    const companyInformation = firebase
+    db
+    .collection('accounts')
+    .where('companyID', '==', companyID)
+    .onSnapshot((snapshot) => {
+      const companyData = snapshot
+      .docs
+      .map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      setCompanyData(companyData)
+    })
+    return () => companyInformation
+  },[])
+
+  //Read Messe
+  React.useEffect(() => {
+    const messeDeltager = firebase
+    db
+    .collection('messe')
+    .where('deltager', 'array-contains', companyID)
+    .onSnapshot((snapshot) => {
+      const messeData = snapshot
+      .docs
+      .map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      setMesseData(messeData)
+    })
+    return () => messeDeltager
+  },[])
 
   // Read Item
   React.useEffect(() => {
@@ -42,23 +78,20 @@ function CompanyPage() {
   },[])
 
     // Add item
-    const addItem = (e) => {
-      e.preventDefault()
+    const addItem = (messeID) => {
       db
       .collection('items')
       .add({
         companyID: companyID,
-        emailID: currentUser.email,
         itemTitle: title,
         itemDesc: desc,
         itemFile: file,
-        messeID: messe,
+        messeID: messeID,
       })
       .then (() => {
         setTitle('')
         setDesc('')
         setFile('')
-        setMesse('')
       })
     }
 
@@ -75,86 +108,83 @@ function CompanyPage() {
   }
 
   return (
-  <div className="grid-x main-area">
-    <div className="cell auto admin-component">
-      <CompanyPage_Banner />
+    <div className="grid-x main-area">
+      {companyData.map (account => (
+        <div className="cell auto admin-component">
+          <CompanyPage_Banner />
 
-      <div className="ownerPage-messe">
-        <div className="ownerPage-messe__action">
-          <h2>MESSE TITEL</h2>
-          <p>NUM</p>
-          <div className="right"> 
-            { currentUser ?  <Link to="/company/egeteknik"><p className="refreshButton"><BsFileEarmarkPlus /></p></Link> : '' }
-            { currentUser ?  <Link to="/company/egeteknik"><p className="deleteButton"><BsTrash/></p></Link> : '' }
-          </div>
-        </div>
-
-        {company.map(companies =>(
-          <div key={companies.id} className="ownerPage-item">
-            <div className="ownerPage-itemAction">
-              <div className="ownerPage-itemAction__doctype">
-                <p className={companies.itemFile}>{companies.itemFile}</p>
-              </div>
-              <div className="ownerPage-item__actions">
-                { currentUser.email == companies.emailID ? <button className="refreshButton"><p><FiRefreshCw /></p></button> : '' }
-                { currentUser.email == companies.emailID ? <button className="deleteButton" onClick={() => deleteItem(companies.id)}><p><BsTrash/></p></button> : '' }
+          <div className="ownerEdit-addItem">
+            <form>
+              <label>Title</label>
+              <input
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              />
+              <label>Description</label>
+              <input
+              value={desc}
+              onChange={e => setDesc(e.target.value)}
+              />
+              <label>File</label>
+              <input
+              value={file}
+              onChange={e => setFile(e.target.value)}
+              />
+              <br/>
+              <br/>
+            </form>
+          </div>  
+  
+          {messeData.map (messe => (
+          <div className="ownerPage-messe">
+            <div className="ownerPage-messe__action">
+              <h2>{messe.messeTitle}</h2>
+              <div className="right"> 
+                { currentUser.email == account.email ?  <Link onClick={() => addItem(messe.messeID)}><p className="refreshButton"><BsFileEarmarkPlus /></p></Link> : '' }
+                { currentUser.email == account.email ?  <Link to="/company/egeteknik"><p className="deleteButton"><BsTrash/></p></Link> : '' }
               </div>
             </div>
-            <div className="ownerPage-item__info">
-              <h5>{companies.itemTitle}</h5>
-              <p>{companies.itemDesc}</p>
-            </div>
-            
-            
-            { currentUser.email == companies.emailID ? '' :
-            <div>
-              <hr/>
-              <div className="ownerPage-item-comment">
-                <div className="ownerPage-item-comment__profileimg">
-                  <img src="http://placekitten.com/50/50" alt=""/>
+  
+            {company.map(items =>( items.messeID == messe.messeID ?
+              <div key={items.id} className="ownerPage-item">
+                <div className="ownerPage-itemAction">
+                  <div className="ownerPage-itemAction__doctype">
+                    <p className={items.itemFile}>{items.itemFile}</p>
+                  </div>
+                  <div className="ownerPage-item__actions">
+                    { currentUser.email == account.email ? <button className="refreshButton"><p><FiRefreshCw /></p></button> : '' }
+                    { currentUser.email == account.email ? <button className="deleteButton" onClick={() => deleteItem(items.id)}><p><BsTrash/></p></button> : '' }
+                  </div>
                 </div>
-
-                <form>
-                  <input type="text" placeholder="Skriv en kommentar..."/>
-                  <button type="submit"><AiOutlineSend/></button>
-                  <button><BsBookmark /></button>
-                </form>
-              </div>
-            </div>
-            }
+                <div className="ownerPage-item__info">
+                  <h5>{items.itemTitle}</h5>
+                  <p>{items.itemDesc}</p>
+                </div>
+                
+                { currentUser.email == account.email ? '' :
+                <div>
+                  <hr/>
+                  <div className="ownerPage-item-comment">
+                    <div className="ownerPage-item-comment__profileimg">
+                      <img src="http://placekitten.com/50/50" alt=""/>
+                    </div>
+  
+                    <form>
+                      <input type="text" placeholder="Skriv en kommentar..."/>
+                      <button type="submit"><AiOutlineSend/></button>
+                      <button><BsBookmark /></button>
+                    </form>
+                  </div>
+                </div>
+                }
+              </div> 
+              : ''
+              ))}
           </div>
-        ))}
-
-        <div className="ownerEdit-addItem">
-          <form>
-            <label>Title</label>
-            <input
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            />
-            <label>Description</label>
-            <input
-            value={desc}
-            onChange={e => setDesc(e.target.value)}
-            />
-            <label>File</label>
-            <input
-            value={file}
-            onChange={e => setFile(e.target.value)}
-            />
-            <label>Messe</label>
-            <input
-            value={messe}
-            onChange={e => setMesse(e.target.value)}
-            />
-            <br/>
-            <br/>
-            <button onClick={addItem}>Create</button>
-          </form>
-        </div>   
-      </div>
+          ))}
+        </div>
+      ))} 
     </div>
-  </div>
   );
 }
 export default CompanyPage;
