9302e1efdfd8e4e47742cbdfc6d3e4bb62100b50
Peter <thewoet@gmail.com>
2020-11-30 12:41:40 +0100

    create owner

--- src/components/pages/CreateOwner.js
+++ src/components/pages/CreateOwner.js
@@ -1,55 +1,112 @@
-import React from 'react'
+import React, {useCallback} from 'react';
+import { withRouter } from 'react-router-dom';
+import firebase from '../../Firebase'
 
-export default function CreateOwner() {
+const CreateOwner = ({ history }) => {
+    const [fname, setName] = React.useState([]);
+    const [adresse, setAdresse] = React.useState([]);
+    const [companyID, setCompanyID] = React.useState([]);
+    const [cvr, setCvr] = React.useState([]);
+    const [phone, setPhone] = React.useState([]);
+    const [desc, setDesc] = React.useState([]);
+    const [logo, setLogo] = React.useState([]);
+    const handleSignUp = useCallback(async event => {
+        event.preventDefault();
+        const {email, password, fname, adresse, companyID, cvr, phone, desc, logo} = event.target.elements;
+        const db = firebase.firestore()
+        const user = firebase.auth()
+        try {
+            await firebase
+            .auth()
+            .createUserWithEmailAndPassword(email.value, password.value)
+            .then(cred => {
+                db.collection('accounts').doc(cred.user.uid).set({
+                  admin: false,
+                  company: true,
+                  fname: fname.value,
+                  adresse: adresse.value,
+                  companyID: companyID.value,
+                  cvr: cvr.value,
+                  phone: phone.value,
+                  desc: desc.value,
+                  logo: logo.value,
+                  userID: cred.user.uid
+                })
+              })
+            history.push ('/admin');
+        
+        }
+        catch (error) {
+            alert(error);
+        }
+    }, [history]);
     return (
         <div>
         <h1>Create Owner Component</h1>
-            <form className="create-owner">
+        <form onSubmit={handleSignUp}>
+              <div id="form">
+              <h1>Sign Up Page</h1>
+                <label for="email">Email</label>
+                <input type="email" name="email" />
                 
-                <div class="grid-container">
-                    <div class="grid-x grid-padding-x">
-                        <div class="medium-6 cell">
-                        <label className="create-owner__header">
-                        <p>Virksomhed</p>
-                            <input type="text" name="name" />
-                            </label>
-                        </div>
-                        <div class="medium-6 cell">
-                            <label className="create-owner__header">
-                            <p>CVR</p>
-                            <input type="text" name="name" />
-                            </label>
-                        </div>
-                        <div class="medium-6 cell">
-                            <label className="create-owner__header">
-                            <p>Email</p>
-                            <input type="text" name="name" />
-                            </label>
-                        </div>
-                        <div class="medium-6 cell">
-                            <label className="create-owner__header">
-                            <p>Telefon Nr</p>
-                            <input type="text" name="name" />
-                            </label>
-                        </div>
-                        <div class="medium-6 cell">
-                            <label className="create-owner__header">
-                            <p>CVR</p>
-                            <input type="text" name="name" />
-                            </label>
-                        </div>
-                        <div class="medium-6 cell">
-                            <label className="create-owner__header">
-                            <p>Logo</p>
-                            <input type="text" name="name" />
-                            </label>
-                        </div>
-                    </div>
-                    <div class="small-12 cell">
-                    <button type="button" class="create-owner__succes">Tilføj Firma</button>
-                    </div>
-                </div>
+                <label for="password">Password</label>
+                <input type="password" name="password" />
+
+                <label for="Navn">Navn</label>
+                <input 
+                    type="text"
+                    name="fname"
+                    value={fname}
+                    onChange={e => setName(e.target.value)} 
+                />
+                <label for="Adresse">Adresse</label>
+                <input 
+                    type="text"
+                    name="adresse"
+                    value={adresse}
+                    onChange={e => setAdresse(e.target.value)} 
+                />
+                <label for="companyID">companyID</label>
+                <input 
+                    type="text"
+                    name="companyID"
+                    value={companyID}
+                    onChange={e => setCompanyID(e.target.value)} 
+                />
+                <label for="cvr">CVR</label>
+                <input 
+                    type="text"
+                    name="cvr"
+                    value={cvr}
+                    onChange={e => setCvr(e.target.value)} 
+                />
+                <label for="phone">phone</label>
+                <input 
+                    type="text"
+                    name="phone"
+                    value={phone}
+                    onChange={e => setPhone(e.target.value)} 
+                />
+                <label for="desc">Beskrivelse</label>
+                <input 
+                    type="text"
+                    name="desc"
+                    value={desc}
+                    onChange={e => setDesc(e.target.value)} 
+                />
+                <label for="desc">Logo</label>
+                <input 
+                    type="text"
+                    name="logo"
+                    value={logo}
+                    onChange={e => setLogo(e.target.value)} 
+                />
+                
+                <input type="submit" value="Sign Up" />
+              </div>
             </form>
         </div>
     )
 }
+
+export default withRouter (CreateOwner);
