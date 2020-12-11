0a03802126ef44e804d0717411457ba7d42646d3
Peter Wind <peterschaadtwind@gmail.com>
2020-12-11 12:24:09 +0100

    tilføjet logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './Header.scss';
+import Logo from'../../assets/IXPOlogoblue.svg';
 import Navigator from './Navigation';
 
 
 function Header() {
   return (
       <div className="grid-x header">
-          <div className="cell small-12 medium-6 header__logo"><Link to="/userpage"><p className="header__logo">Codename IXPO</p></Link></div>
-          <div className="cell small-12 medium-6">
+          <div className="cell small-12 medium-2 header__logo"><Link to="/userpage"><img src={Logo} alt="React Logo" className="logosvg" /></Link></div>
+          <div className="cell small-12 medium-10">
               <Navigator />
           </div>
       </div>
